51aa14cc1aa11fda1fca55e69450f19ac271f9ab
minhhieple <hieplevuc@gmail.com>
2020-12-10 15:55:30 +0700

    update

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ app.post('/webhook', (req, res) => {
 
     // Parse the request body from the POST
     let body = req.body;
-    // console.log(req.body);
+    console.log(req.body);
     // Check the webhook event is from a Page subscription
     if (body.object === 'page') {
 
